258948cfcf3bb3687b4cd74795695d2a6b5dbddf
sfasulo <spencerf0123@gmail.com>
2023-10-13 23:01:36 -0400

    Figuring out Angular. Added Bootstrap.

---
+++ frontend/AI-Hackathon/src/app/chat-window/chat-window.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-window',
+  templateUrl: './chat-window.component.html',
+  styleUrls: ['./chat-window.component.less']
+})
+export class ChatWindowComponent {
+
+}
