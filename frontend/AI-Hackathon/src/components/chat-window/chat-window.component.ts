f7e2dceb0948cf64a01280ed7945b9e7f314c031
Sn00pyW00dst0ck <gabealdous@gmail.com>
2023-10-14 12:47:51 -0400

    Adding sample text to speech

--- frontend/AI-Hackathon/src/components/chat-window/chat-window.component.ts
+++ frontend/AI-Hackathon/src/components/chat-window/chat-window.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiService } from 'src/services/api/api.service';
 import { AudioPlayerService } from 'src/services/audio-player/audio-player.service';
+import { SpeechToTextService } from 'src/services/speech-to-text/speech-to-text.service';
 
 /**
  * Displays a chat window
@@ -13,19 +14,26 @@ import { AudioPlayerService } from 'src/services/audio-player/audio-player.servi
 export class ChatWindowComponent {
   text = "";
 
-  constructor(private api: ApiService, private audioPlayer: AudioPlayerService)  {
+  constructor(private api: ApiService, private audioPlayer: AudioPlayerService, private speechRecognizer: SpeechToTextService) {
 
     // Sample of how to use API Service
-    this.api.getHelloWorld().subscribe((data) =>  {
+    this.api.getHelloWorld().subscribe((data) => {
       this.text = data;
       console.log(this.text);
     });
+
+    this.speechRecognizer.observeRecognizedText().subscribe((data) => {
+      console.log(data);
+    });
+
+    this.speechRecognizer.start();
+
   }
 
-  private playSound()  {
+  private playSound() {
     // Sample of how to use audio player service
     this.audioPlayer.setAudioSource("assets/mixkit-arcade-retro-game-over-213.wav");
-    this.audioPlayer.observeAudioState().subscribe((data) =>  {
+    this.audioPlayer.observeAudioState().subscribe((data) => {
       console.log(data);
     });
     this.audioPlayer.play();
