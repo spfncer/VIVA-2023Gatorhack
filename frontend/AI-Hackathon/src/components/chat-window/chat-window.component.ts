03a4b580558782d54954c45a12776ffcdaed2d9e
Sn00pyW00dst0ck <gabealdous@gmail.com>
2023-10-15 15:07:03 -0400

    Working Transcript.

--- frontend/AI-Hackathon/src/components/chat-window/chat-window.component.ts
+++ frontend/AI-Hackathon/src/components/chat-window/chat-window.component.ts
@@ -13,21 +13,27 @@ import { SpeechToTextService } from 'src/services/speech-to-text/speech-to-text.
   styleUrls: ['./chat-window.component.less']
 })
 export class ChatWindowComponent {
-  text = "";
+  transcript = "";
+  promptText = "";
   viseme: any;
   waiting = false;
 
-  private wasPlaying = false;
-  constructor(private api: ApiService, private audioPlayer: AudioPlayerService, private speechRecognizer: SpeechToTextService) {
+  hasInitialized = false;
 
+  private wasPlaying = false;
+
+  constructor(private api: ApiService, private audioPlayer: AudioPlayerService, private speechRecognizer: SpeechToTextService) {
     // Sample of how to use API Service
     this.api.getHelloWorld().subscribe((data) => {
-      this.text = data;
-      console.log(this.text);
+      this.promptText = data;
+      console.log(this.promptText);
     });
 
     this.speechRecognizer.observeRecognizedText().subscribe((data) => {
-      this.text = data;
+      this.promptText = data;
+      if (!this.promptText) {
+        this.promptText = '';
+      }
     });
 
     this.audioPlayer.observeAudioState().subscribe((state: AudioStreamState) => {
@@ -45,10 +51,13 @@ export class ChatWindowComponent {
 
       if (value == false) {
         this.waiting = true;
-        this.api.postQuery(this.text).subscribe((data: any) => {
+        this.transcript += "You: " + this.promptText + '\n';
+        this.api.postQuery(this.promptText).subscribe((data: any) => {
+          this.promptText = '';
           const stringifiedData = JSON.stringify(data);
           // Parse from JSON
           const parsedJson = JSON.parse(stringifiedData);
+          this.transcript += "Viva: " + parsedJson.text + '\n';
           this.viseme = parsedJson.viseme;
           // Play Audio
           const url = "data:audio/wav;base64," + parsedJson.audio;
